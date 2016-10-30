fd0285f608c55ea5ceed2f98a4e50d75f2e04cd0
Irit Goihman <igoihman@redhat.com>
2016-10-30 18:37:16 +0200

    updated scripts

--- install-ovs-2.5-fedora.sh
+++ install-ovs-2.5-fedora.sh
@@ -8,3 +8,18 @@ git checkout branch-2.5
 ./configure
 cp openvswitch-2.5.2.tar.gz $HOME/rpmbuild/SOURCES
 rpmbuild -bb --with dpdk --without check rhel/openvswitch-fedora.spec
+
+# DB creation
+mkdir -p /usr/local/etc/openvswitch
+mkdir -p /usr/local/var/run/openvswitch
+rm /usr/local/etc/openvswitch/conf.db
+ovsdb-tool create /usr/local/etc/openvswitch/conf.db  \
+	/usr/local/share/openvswitch/vswitch.ovsschema
+
+# start ovs-db server, no ssl support
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+	--remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+	--pidfile --detach
+
+# init DB
+ovs-vsctl --no-wait init
